ec1dee06e0a8c095120eb04cddf60724833016d3
Matheus Pedroni <pedroni.dev@gmail.com>
2018-04-04 01:12:50 -0300

    First commit

---
+++ pacman.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char mapa[10][10];
+int pacX, pacY;
+
+int score = 0;
+
+void readMap() {
+
+    FILE *file;
+
+    file = fopen("mapa.txt", "r");
+
+    int i, j;
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            fscanf(file, "%c", &mapa[i][j]);
+            if (mapa[i][j] == '@') {
+                pacX = i;
+                pacY = j;
+            }
+        }
+    }
+
+}
+
+void printMap() {
+    printf("\nSCORE = %d\n", score);
+    printf("\n");
+    int i, j;
+    for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
+            printf("%c", mapa[i][j]);
+        }
+    }
+    fflush(stdout);
+}
+
+int isParede(char position) {
+    if (position == '|' || position == '-') {
+        return 1;
+    }
+    return 0;
+}
+
+void checkScore() {
+
+}
+
+void readJogada() {
+
+    char jogada;
+
+    while (scanf("%c", &jogada) != EOF) {
+        if (jogada == 'd' && !isParede(mapa[pacY][pacX + 1])) {
+            mapa[pacY][pacX] = '\0';
+            pacX++;
+            mapa[pacY][pacX] = '@';
+
+        } else if (jogada == 'a' && !isParede(mapa[pacY][pacX - 1])) {
+            mapa[pacY][pacX] = '\0';
+            pacX--;
+            mapa[pacY][pacX] = '@';
+
+        } else if (jogada == 'w' && !isParede(mapa[pacY - 1][pacX])) {
+            mapa[pacY][pacX] = '\0';
+            pacY--;
+            mapa[pacY][pacX] = '@';
+
+        } else if (jogada == 's' && !isParede(mapa[pacY + 1][pacX])) {
+            mapa[pacY][pacX] = '\0';
+            pacY++;
+            mapa[pacY][pacX] = '@';
+
+        }
+        printMap();
+    }
+
+}
+
+int main() {
+    readMap();
+    printMap();
+    readJogada();
+    return 0;
+}
